7cb18ed6893e1b0cce00b6f856a4d34ad1b03bb0
Esey-M <eseymhretab@hotmail.com>
2025-03-25 11:56:16 +0100

    Deactivate quiz feature and add coming soon page

--- habesha/src/Controller/QuizController.php
+++ habesha/src/Controller/QuizController.php
@@ -16,77 +16,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuizController extends AbstractController
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
-        private QuizRepository $quizRepository
+        private QuizRepository $quizRepository,
+        private EntityManagerInterface $entityManager
     ) {}
 
     #[Route('/', name: 'app_quiz')]
     public function index(): Response
     {
-        return $this->render('quiz/index.html.twig', [
-            'quizzes' => $this->quizRepository->findBy([], ['createdAt' => 'DESC'])
-        ]);
+        return $this->render('quiz/index.html.twig');
     }
 
     #[Route('/new', name: 'app_quiz_new')]
-    public function new(Request $request): Response
+    public function new(): Response
     {
-        $quiz = new Quiz();
-        $form = $this->createForm(QuizType::class, $quiz);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($quiz);
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('app_quiz_show', ['id' => $quiz->getId()]);
-        }
-
-        return $this->render('quiz/new.html.twig', [
-            'form' => $form
-        ]);
+        return $this->redirectToRoute('app_quiz');
     }
 
     #[Route('/{id}', name: 'app_quiz_show')]
     public function show(Quiz $quiz): Response
     {
-        return $this->render('quiz/show.html.twig', [
-            'quiz' => $quiz
-        ]);
+        return $this->redirectToRoute('app_quiz');
     }
 
     #[Route('/{id}/take', name: 'app_quiz_take')]
     public function take(Quiz $quiz): Response
     {
-        return $this->render('quiz/take.html.twig', [
-            'quiz' => $quiz
-        ]);
+        return $this->redirectToRoute('app_quiz');
     }
 
     #[Route('/{id}/submit', name: 'app_quiz_submit', methods: ['POST'])]
     public function submit(Request $request, Quiz $quiz): Response
     {
-        $answers = $request->request->all('answers');
-        $score = 0;
-        $totalQuestions = count($quiz->getQuestions());
-
-        foreach ($answers as $questionIndex => $answer) {
-            $question = $quiz->getQuestions()->get($questionIndex);
-            if ($question && $answer == $question->getCorrectOption()) {
-                $score++;
-            }
-        }
-
-        $result = new QuizResult();
-        $result->setQuiz($quiz);
-        $result->setScore($score);
-        $result->setAnswers($answers);
-        $result->setCompletedAt(new \DateTimeImmutable());
-
-        $this->entityManager->persist($result);
-        $this->entityManager->flush();
-
-        return $this->redirectToRoute('app_quiz_result', ['id' => $result->getId()]);
+        return $this->redirectToRoute('app_quiz');
     }
 
     #[Route('/result/{id}', name: 'app_quiz_result')]
